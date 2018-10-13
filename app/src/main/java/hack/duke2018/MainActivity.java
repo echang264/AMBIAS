56fe3cad459a7fa2a633ef699399a70cf9bc1cc9
Eric Chang <etc18@duke.edu>
2018-10-13 18:26:54 -0400

    nat/loc and topics

--- app/src/main/java/hack/duke2018/MainActivity.java
+++ app/src/main/java/hack/duke2018/MainActivity.java
@@ -1,5 +1,6 @@
 package hack.duke2018;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,28 +9,45 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button1;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        button1 = (Button) findViewById(R.id.localButton);
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                openLocalPositions();
+            }
+        });
+        button2 = (Button) findViewById(R.id.nationalButton);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNationalPositions();
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public void openLocalPositions() {
+        Intent intent = new Intent(this, LocalPositions.class);
+        startActivity(intent);
+    }
+
+    public void openNationalPositions() {
+        Intent intent = new Intent(this, NationalPositions.class);
+        startActivity(intent);
+    }
+}
+ /*
+   @Override
+     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
@@ -50,3 +68,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+*/
